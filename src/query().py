54a582c6b3878e1839508f0b6c73f19ae29a54e0
vijethabathala <99159890+vijethabathala@users.noreply.github.com>
2022-02-22 12:03:05 +0530

    Update query().py

--- src/query().py
+++ src/query().py
@@ -1,6 +1,10 @@
 """
 @author: sparkbyexamples.com
+For complete example refer to 
+https://sparkbyexamples.com/pandas/pandas-query[]-method-with-examples
+
 """
+
 #Pandas.DataFrame.query() by Examples
 import pandas as pd
 import numpy as np
@@ -12,28 +16,36 @@ technologies= {
           }
 df = pd.DataFrame(technologies)
 print(df)
+
 # Query all rows with Courses equals 'Spark'
 df2=df.query("Courses == 'Spark'")
 print(df2)
+
 # Query Rows by using Python variable
 value='Spark'
 df2=df.query("Courses == @value")
 print(df2)
+
 # Replace current esisting DataFrame
 df=pd.DataFrame(technologies)
 df2=df.query("Courses == 'Spark'",inplace=True)
 print(df2)
+
 # not equals condition
 df=pd.DataFrame(technologies)
 df2=df.query("Courses != 'Spark'")
 print(df2)
+
 # Query Rows by list of values
 print(df.query("Courses in ('Spark','PySpark')"))
+
 # Query Rows by list of values
 values=['Spark','PySpark']
 print(df.query("Courses in @values"))
+
 # By using lambda function
 print(df.apply(lambda row: row[df['Courses'].isin(['Spark','PySpark'])]))
+
 # Other examples you can try to query rows
 df2=df.loc[(df['Discount'] >= 1000) & (df['Discount'] <= 2000)]
 print(df2)
