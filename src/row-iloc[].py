b965648174fbaec37e892bfd1939b43cf2ace0b8
vijethabathala <99159890+vijethabathala@users.noreply.github.com>
2022-02-22 13:25:15 +0530

    Update and rename iloc.py to row-iloc[].py

--- src/row-iloc[].py
+++ src/row-iloc[].py
@@ -1,6 +1,8 @@
 #Pandas Select Rows by Index position
 """
 @author: sparkbyexamples.com
+For complete example refer to 
+https://sparkbyexamples.com/pandas/pandas-select-rows-using-iloc[]-with-examples
 
 """
 import pandas as pd
@@ -13,21 +15,29 @@ technologies = {
                }
 index_labels=['r1','r2','r3','r4','r5','r6','r7']
 df = pd.DataFrame(technologies,index=index_labels)
+
 # Use pandas.DataFrame.iloc[] to Select Rows by Integer Index
 # Select Row by Integer Index
 print(df.iloc[2])
+
 # Select Rows by Index List
 print(df.iloc[[2,3,6]])
+
 # Select Rows by Integer Index Range
 print(df.iloc[1:5])
+
 # Select First Row by Index
 print(df.iloc[:1])
+
 # Select First 3 Rows
 print(df.iloc[:3])
+
 # Select Last Row by Index
 print(df.iloc[-1:])
+
 # Select Last 3 Row
 print(df.iloc[-3:])
+
 # Selects alternate rows
 print(df.iloc[::2])
 
