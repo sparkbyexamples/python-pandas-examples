d01ac160ab6c8b4f4cb5344de48575ac9a0e5dbe
vijethabathala <99159890+vijethabathala@users.noreply.github.com>
2022-02-25 19:56:47 +0530

    Update get-column-names.py

--- get-column-names.py
+++ get-column-names.py
@@ -1,5 +1,7 @@
 """
 @author: sparkbyexamples.com
+For complete examples refer to
+https://sparkbyexamples.com/pandas-get-column-names-usage-with-examples
 
 """
 # Pandas get column names from dataframe
